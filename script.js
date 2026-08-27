ac9ea762a194a4c5e895a9c35e5ef14aad79b570
koderph0526-coder <koderph0526@gmail.com>
2026-08-27 13:15:37 +0200

    RMnotesUpdate_CssCleanedUpAbit_JsNotes

---
+++ script.js
@@ -0,0 +1,6 @@
+// Fix switch case or if/else to make submit button inactive until valid email is added
+const emailInput = document.querySelector(".emailSubmit");
+const submitBtn = document.querySelector(".btnSubmit");
+
+//Also add a connection between the checkboxes tot heir corresponding options!
+//Might be simpler to create the checkboxes here and append them to their respective options?
