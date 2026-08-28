8bb5785cd6064da05b49a0787e71bb7a0f1f6071
koderph0526-coder <koderph0526@gmail.com>
2026-08-28 10:46:00 +0200

    ProofOfConcept_LandingPageDone

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const submitBtn = document.querySelector(".btnSubmit");
 
 //Also add a connection between the checkboxes tot heir corresponding options!
 //Might be simpler to create the checkboxes here and append them to their respective options?
+
+// Add  a function to alter nav a bit(making it smaller) and have it export to the other js files that is to be attached to the childPages?
